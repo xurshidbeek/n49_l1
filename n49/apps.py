8f0c6ae0f0c38d62c8f6ebbeb8975545a17fdb7d
xurshidbeek <fezotjonuzadi@gmail.com>
2024-08-28 20:42:39 +0500

    l_1

---
+++ n49/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class N49Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'n49'
---
+++ n49/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'temp_1.html')
+# Create your views here.
